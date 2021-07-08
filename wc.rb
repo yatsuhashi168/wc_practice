ba5317e794a932e2aa58fe6b7cb54dbb69d3854a
yatsuhashi168 <kurimi777luk@gmail.com>
2021-07-08 18:25:37 +0900

    オプション-l、ファイルが複数の場合に対応

--- wc.rb
+++ wc.rb
@@ -3,15 +3,40 @@
 require 'optparse'
 
 def main
-  number_of_line = IO.read(ARGV[0]).count("\n")
-  number_of_word = IO.read(ARGV[0]).split(' ').size
-  size = IO.read(ARGV[0]).size
-  print number_of_line.to_s.rjust(8)
-  print number_of_word.to_s.rjust(8)
-  print size.to_s.rjust(8)
-  print ARGV[0]
+  option = { l: false }
+  opt = OptionParser.new
+  opt.on('-l', 'show number of lines') { option[:l] = true }
+  opt.parse!(ARGV)
+
+  number_of_lines = ARGV.map { |line| IO.read(line).count("\n") }
+  number_of_words = ARGV.map { |word| IO.read(word).split(' ').size }
+  sizes = ARGV.map { |size| IO.read(size).size }
+
+  if option[:l]
+    ARGV.size.times do |i|
+      print number_of_lines[i].to_s.rjust(8)
+      print " #{ARGV[i]}"
+      puts
+    end
+    if ARGV.size != 1
+      print number_of_lines.sum.to_s.rjust(8)
+      print ' total'
+    end
+  else
+    ARGV.size.times do |i|
+      print number_of_lines[i].to_s.rjust(8)
+      print number_of_words[i].to_s.rjust(8)
+      print sizes[i].to_s.rjust(8)
+      print " #{ARGV[i]}"
+      puts
+    end
+    if ARGV.size != 1
+      print number_of_lines.sum.to_s.rjust(8)
+      print number_of_words.sum.to_s.rjust(8)
+      print sizes.sum.to_s.rjust(8)
+      print ' total'
+    end
+  end
 end
 
 main
-
-# p File.open(ARGV[0]).read.count("\n")
