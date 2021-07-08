8f1fd02b1271cc00149ddf4d8fc3523bd89c4f1f
yatsuhashi168 <kurimi777luk@gmail.com>
2021-07-08 15:53:48 +0900

    オプションなし、ファイル1つの場合に対応

--- wc.rb
+++ wc.rb
@@ -1,6 +1,17 @@
 # frozen_string_literal: true
 
+require 'optparse'
+
 def main
-  p ARGV
-  IO.foreach(ARGV) { |x| print 'got', x }
+  number_of_line = IO.read(ARGV[0]).count("\n")
+  number_of_word = IO.read(ARGV[0]).split(' ').size
+  size = IO.read(ARGV[0]).size
+  print number_of_line.to_s.rjust(8)
+  print number_of_word.to_s.rjust(8)
+  print size.to_s.rjust(8)
+  print ARGV[0]
 end
+
+main
+
+# p File.open(ARGV[0]).read.count("\n")
